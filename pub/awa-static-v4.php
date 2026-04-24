2304e75023d0b9f3a7b4c704299ccd72a2eb2f52
Jess <teste@awamotos.com.br>
2026-04-24 14:11:34 +0000

    security: remover scripts de diagnostico expostos em pub/

--- pub/awa-static-v4.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-http_response_code(410);
-header('Content-Type: text/plain; charset=utf-8');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-
-echo "This endpoint has been retired.\n";
-echo "Use Magento CLI / controlled deployment tasks instead.\n";
