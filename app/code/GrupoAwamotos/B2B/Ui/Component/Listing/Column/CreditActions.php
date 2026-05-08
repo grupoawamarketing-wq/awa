f2f4079a43f19cd2d82301fb0c6fda38e08cef0f
Jess <teste@awamotos.com.br>
2026-05-08 18:00:07 +0000

    fix(b2b): replace non-existent EditAction with custom actions columns
    - Create CompanyActions column class (extends Magento\Ui\Component\Listing\Columns\Column)
    - Create CreditActions column class (extends Magento\Ui\Component\Listing\Columns\Column)
    - Fix company_listing.xml: was using Magento\Ui\Component\Listing\Columns\EditAction (non-existent)
    - Fix credit_listing.xml: was using Magento\Ui\Component\Listing\Columns\EditAction (non-existent)
    - Resolves ReflectionException on Company/Index and Credit admin pages

---
+++ app/code/GrupoAwamotos/B2B/Ui/Component/Listing/Column/CreditActions.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GrupoAwamotos\B2B\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class CreditActions extends Column
+{
+    private const URL_PATH_EDIT = 'grupoawamotos_b2b/credit/edit';
+
+    protected UrlInterface $urlBuilder;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource): array
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                if (isset($item['entity_id'])) {
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                self::URL_PATH_EDIT,
+                                ['id' => $item['entity_id']]
+                            ),
+                            'label' => __('Editar'),
+                        ],
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
